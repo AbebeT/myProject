63a7eab64141cd2927d8adf47b250029fa2a4f76
Abebe Tekle Abrha <abe.habesha@gmail.com>
2021-12-28 17:35:50 -0600

    application properties add

--- src/main/java/com/example/myproject/MyProjectApplication.java
+++ src/main/java/com/example/myproject/MyProjectApplication.java
@@ -2,12 +2,22 @@ package com.example.myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class MyProjectApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(MyProjectApplication.class, args);
+    }
+
+    @RequestMapping
+    public String greeetings(){
+        return "Hello there";
     }
 
 }
